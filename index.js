0ab3224a1412cf66a3327b0dafb0df24639467e1
ESTHER WANJIKU <ESTHERWANJIQU@GMAIL.COM>
2023-12-12 13:34:20 +0300

    aster

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
-// Write your code here
+var num1=2;
+var num2=31;
+var multiply=(num1*num2);
+var random=Math.random();
+var num3=(Math.random()+4);
+var num4=(Math.random());
+var mod=(num3%num4);
+var max=(Math.max[12,10,20,15]);
+
+
